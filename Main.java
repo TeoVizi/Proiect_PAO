8527b95904c47f4bc910691870eccd2cdabc4c31
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2024-04-02 23:35:37 +0300

    clasa Meniu + verificari

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Utilizator utilizator = new Utilizator("George Ionescu", "george.ionescu@email.com");
 
-            //verificari Utilizator
+        //verificari Utilizator
         System.out.println(utilizator.getNume());
         System.out.println(utilizator.getEmail());
 
@@ -12,27 +12,27 @@ public class Main {
         System.out.println(utilizator.getNume());
         System.out.println(utilizator.getEmail());
 
-            //verificari Client
+        //verificari Client
         Client client = new Client("Mihai Popescu", "mihai.popescu@email.com", "Strada Libertatii", "10", "Bucuresti", true);
 
-        System.out.println( client.getAdresaCompleta());
+        System.out.println(client.getAdresaCompleta());
 
         System.out.println(client.getNume());
         System.out.println(client.getEmail());
-        System.out.println( client.getStrada());
-        System.out.println( client.getNumar());
-        System.out.println( client.getOras());
-        System.out.println( client.getIsPremium());
+        System.out.println(client.getStrada());
+        System.out.println(client.getNumar());
+        System.out.println(client.getOras());
+        System.out.println(client.getIsPremium());
 
         client.setStrada("Strada Unirii");
         client.setNumar("20A");
         client.setOras("Cluj-Napoca");
         client.setPremium(false);
-        System.out.println( client.getIsPremium());
+        System.out.println(client.getIsPremium());
 
         System.out.println(client.getAdresaCompleta());
 
-            //verificari ItemMeniu
+        //verificari ItemMeniu
 
         ItemMeniu pizzaMargherita = new ItemMeniu("Pizza Margherita", "Pizza", 35.0);
 
@@ -43,6 +43,20 @@ public class Main {
         pizzaMargherita.setDescriere("Pizza bombastica");
         System.out.println(pizzaMargherita.getDescriere());
 
+        // verificaro Meniu
 
+        Meniu meniu = new Meniu();
+
+        meniu.adaugaItemMeniu(new ItemMeniu("Pizza Margherita", "Pizza", 35.0));
+        meniu.adaugaItemMeniu(new ItemMeniu("Lasagna", "Lasagn", 45.0));
+        meniu.adaugaItemMeniu(new ItemMeniu("Tiramisu", "Tiramisu", 25.0));
+
+
+        for (ItemMeniu item : meniu.getItemiMeniu()) {
+            System.out.println(item.getNume());
+            System.out.println(item.getDescriere());
+            System.out.println(item.getPret() + " lei");
+            System.out.println();
+        }
     }
 }
---
+++ Meniu.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Meniu {
+    private List<ItemMeniu> listaItemiMeniu;
+
+    public Meniu() {
+        this.listaItemiMeniu = new ArrayList<>();
+    }
+
+    public void adaugaItemMeniu(ItemMeniu item) {
+        listaItemiMeniu.add(item);
+    }
+
+    public List<ItemMeniu> getItemiMeniu() {
+        return listaItemiMeniu;
+    }
+}
