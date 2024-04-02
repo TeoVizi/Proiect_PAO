bf1141508194f9980476090b14b492eab6298d8c
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2024-04-03 00:31:39 +0300

    am mai adaugat chestii la Item

--- Item.java
+++ Item.java
@@ -7,11 +7,25 @@ public abstract class Item {
         this.descriere = descriere;
     }
 
+    public Item() {
+        // Constructor implicit fără parametri
+        this.nume = null;
+        this.descriere = null;
+    }
+
     public String getNume() {
         return nume;
     }
 
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
     public String getDescriere() {
         return descriere;
+    }
+
+    public void setDescriere(String descriere) {
+        this.descriere = descriere;
     }
 }
