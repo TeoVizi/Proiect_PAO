f44b7901ce1eb956f7d349efba3c068b2f8ad784
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2024-04-02 22:48:20 +0300

    mai multe verificari pentru Client

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Utilizator utilizator = new Utilizator("George Ionescu", "george.ionescu@email.com");
 
+            //verificari Utilizator
         System.out.println(utilizator.getNume());
         System.out.println(utilizator.getEmail());
 
@@ -10,5 +11,22 @@ public class Main {
 
         System.out.println(utilizator.getNume());
         System.out.println(utilizator.getEmail());
+
+            //verificari Client
+        Client client = new Client("Mihai Popescu", "mihai.popescu@email.com", "Strada Libertatii", "10", "Bucuresti");
+
+        System.out.println( client.getAdresaCompleta());
+
+        System.out.println( client.getNume());
+        System.out.println( client.getEmail());
+        System.out.println( client.getStrada());
+        System.out.println( client.getNumar());
+        System.out.println( client.getOras());
+
+        client.setStrada("Strada Unirii");
+        client.setNumar("20A");
+        client.setOras("Cluj-Napoca");
+
+        System.out.println(client.getAdresaCompleta());
     }
 }
