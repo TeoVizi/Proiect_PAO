881d87994c754ba6897dd596ec6db38fdd5ea366
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2024-04-02 22:32:55 +0300

    verificari in Main ale clasei simple Utilizator

--- Main.java
+++ Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Utilizator utilizator = new Utilizator("George Ionescu", "george.ionescu@email.com");
+
+        System.out.println(utilizator.getNume());
+        System.out.println(utilizator.getEmail());
+
+        utilizator.setNume("George Alin Ionescu");
+        utilizator.setEmail("george.a.ionescu@email.com");
+
+        System.out.println(utilizator.getNume());
+        System.out.println(utilizator.getEmail());
+    }
 }
